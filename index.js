add4605d910599d274cdc731b6f80a73d497df4c
Felipe Vieira <flpchapola@hotmail.com>
2017-10-02 00:43:22 -0300

    linkado mongodb, gravando restaurante e listando restaurante

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const port = process.eventNames.PORT || 3000
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded())
+
+let database = {}
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+const insert = (db, collectionName, doc) => {
+	return new Promise ((resolve,reject) => {
+		const  collection = db.collection(collectionName)
+		collection.insert(doc, (err,result) => {
+			if(err){
+				reject(err)
+			}else{
+				resolve(result)
+			}
+		})
+	})
+}
+
+
+const find = (db, collectionName, filter) => {
+	return new Promise ((resolve,reject) => {
+		const  collection = db.collection(collectionName)
+		const cursor = collection.find(filter)
+		const results = []
+		cursor.forEach( doc => results.push(doc),
+			err => {
+			if(err){
+				reject(err)
+			}else{
+				resolve(results)
+			}
+		})
+	})
+}
+
+
+
+app.get('/', (req,res) => res.render('index'))
+
+app.get('/restaurantes', async(req,res) => {
+	const restaurantes = await find(database, 'restaurantes', {})
+	res.render('restaurantes', { restaurantes })
+})
+
+app.get('/restaurantes/novo', (req,res) => {
+	res.render('restaurante_novo')
+})
+
+
+app.post('/restaurantes/novo', async(req,res) => {
+	const restaurante = {
+		nome: req.body.nome
+	}
+	console.log(req.body)
+	await insert(database, 'restaurantes', restaurante)
+	res.redirect('/restaurantes')
+})
+
+MongoClient.connect('mongodb://localhost:27017/tanamesa', (err,db) => {
+	database = db
+	app.listen(port, () => console.log('Ta na mesa server running'))
+})
